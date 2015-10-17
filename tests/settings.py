8fe3d0c4f50e747f61784f6af6aebc4ce6602939
Brandon Taylor <alsoicode@gmail.com>
2015-10-17 16:33:58 -0400

    Moved models into separate module so their tables will be created by the test runner. Added test to ensure data is persisted.

--- tests/settings.py
+++ tests/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
 
     'fontawesome',
+    'tests',
 )
 
 MIDDLEWARE_CLASSES = (
--- tests/test_fields.py
+++ tests/test_fields.py
@@ -2,51 +2,22 @@
 from __future__ import absolute_import, unicode_literals
 
 from django import forms
-from django.db import models
 from django.test import TestCase
-from django.utils.encoding import python_2_unicode_compatible
 
-from fontawesome.fields import IconField
 from fontawesome.utils import get_icon_choices
+from .models import GoodWidget, SpecificWidget, ExcludeWidget
 
 
-@python_2_unicode_compatible
-class GoodWidget(models.Model):
-    title = models.CharField(max_length=100)
-    icon = IconField()
-
-    def __str__(self):
-        return self.title
-
-
-class WidgetForm(forms.ModelForm):
+class GoodWidgetForm(forms.ModelForm):
 
     class Meta:
         model = GoodWidget
-
-
-@python_2_unicode_compatible
-class SpecificWidget(models.Model):
-    title = models.CharField(max_length=100)
-    icon = IconField(only_ids=['music', 'heart', 'star'])
-
-    def __str__(self):
-        return self.title
 
 
 class SpecificWidgetForm(forms.ModelForm):
 
     class Meta:
         model = SpecificWidget
-
-
-@python_2_unicode_compatible
-class ExcludeWidget(models.Model):
-    title = models.CharField(max_length=100)
-    icon = IconField(exclude_ids=['music', 'heart', 'star'])
-
-    def __str__(self):
-        return self.title
 
 
 class ExcludeWidgetForm(forms.ModelForm):
@@ -59,12 +30,17 @@ class IconFieldTestCases(TestCase):
 
     def test_all_icons(self):
         all_icons = get_icon_choices()
-        good_widget_form = WidgetForm()
+        good_widget_form = GoodWidgetForm()
 
         self.assertEqual(all_icons,
             good_widget_form.fields['icon'].widget.choices,
             u"good_widget_form.icon field choices should have the full set of "
             U"icons")
+
+    def test_data_persisted(self):
+        widget = GoodWidgetForm({'title': 'Test', 'icon': 'music'}).save()
+        self.assertEqual(widget.icon.id, 'music', u'widget.icon should equal '
+            u'"music". Instead equals: {}'.format(widget.icon.id))
 
     def test_only_specific_ids_included(self):
         specific_widget_form = SpecificWidgetForm()
